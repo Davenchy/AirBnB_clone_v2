5fef709593003e08e6fc8c0a0749fe5a247c60b4
Fadi Asaad <firon1222@gmail.com>
2023-08-21 15:31:39 +0300

    create classes_injector util class

---
+++ models/utils/classes_injector.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""A classes injector for the defined models"""
+
+
+class ClassesInjector:
+    """Register classes once and inject them every where"""
+    _classes = {}
+
+    def register(self, *args, **kwargs):
+        """Register a new class
+
+        Example:
+            >>> injector.register(Class1, MyClass=Class2, Class3)"""
+        for cls in args:
+            self._classes[cls.__name__] = cls
+        for k, v in kwargs.items():
+            self._classes[k] = v
+
+    @property
+    def classes(self):
+        """Returns a copy of the registered classes
+
+        Returns: dict"""
+        return self._classes.copy()
+
+    @property
+    def keys(self):
+        """Return classes keys
+
+        Returns: List"""
+        return self._classes.keys()
+
+    def hasClass(self, name):
+        """Check if class is registered
+
+        Args:
+            name (str): class name
+
+        Returns: bool"""
+        return name in self._classes
+
+    def __getitem__(self, name):
+        """Return a class by name """
+        return self._classes[name]
+
+    def __setitem__(self, name, value):
+        """Set a class by name """
+        self._classes[name] = value
