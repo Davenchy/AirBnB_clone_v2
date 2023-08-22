6178291e812b13dc1aa353a458a06dd0f109bfd3
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-22 12:55:28 +0000

    try to fix task#10

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+import models.place  # !NOTE: required for ORM
 
 
 class Amenity(BaseModel, Base):
