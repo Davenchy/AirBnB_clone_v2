03cb74e10fac85f6b90879ee8720300ffbf56b72
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-23 09:59:39 +0000

    try to fix task2

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,9 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+import models
 
 Base = declarative_base()
 
@@ -23,13 +23,11 @@ class BaseModel:
             self.updated_at = datetime.now()
 
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
+            for key, value in kwargs.items():
+                if key=='created_at' or key=='updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
 
@@ -68,6 +66,7 @@ class BaseModel:
 
     def delete(self):
         """Delete the current instance from the storage"""
+        from models import storage
         storage.delete(self)
 
     @property
