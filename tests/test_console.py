05047d199f07f6863d225b328e676c7971c9bd44
Fadi Asaad <firon1222@gmail.com>
2023-08-24 15:39:01 +0300

    solve pystylecode warnings, add tests file for the console

---
+++ tests/test_console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Unit tests for the console """
+from unittest import TestCase
+
+model = __import__('console')
+
+
+class test_Console(TestCase):
+    """ Test Case For The Console """
+    pass
