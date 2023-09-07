beda048820315bb8e5d4b561cfb9bd5f9809fccb
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-07 08:28:37 +0000

    add task0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# sets up my web servers for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Alx School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+
+sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+
+exit 0
