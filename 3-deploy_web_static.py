4ef9cdb1113e43b8671eb46d98b1839de110464e
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-07 14:32:04 +0000

    add task3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 """
-This module deploys the web_static archive to the servers.
+This module fully deploys web_static
 """
-
+from datetime import datetime
 from os.path import exists
 from fabric.api import *
 import shlex
 
 env.hosts = ['54.237.43.214', '54.208.103.110']
 env.user = 'ubuntu'
+
+
+def do_pack():
+    """Pack web_static."""
+    try:
+        if not exists('versions'):
+            local('mkdir versions')
+        now = datetime.now()
+        arc_path = f"versions/web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
+        local(f'tar -czvf {arc_path} web_static')
+        return arc_path
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -40,3 +53,15 @@ def do_deploy(archive_path):
         return True
     except Exception:
         return False
+
+
+def deploy():
+    """Fully deploying web_static."""
+    try:
+        archive_path = do_pack()
+        if (archive_path is None):
+            return False
+    except Exception:
+        return False
+
+    return do_deploy(archive_path)
