2bd8a10808aa05c6492533afe9ca7b3937dcef66
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-07 14:24:11 +0000

    fix2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,31 +6,38 @@ This module deploys the web_static archive to the servers.
 from os.path import exists
 from fabric.api import *
 import shlex
+from fabric import task
 
 env.hosts = ['54.237.43.214', '54.208.103.110']
 env.user = 'ubuntu'
 
 
+@task
 def do_deploy(archive_path):
     """Deploy web_static."""
     if not exists(archive_path):
         return False
 
     try:
-        archive_name = archive_path.split('/')[-1].split('.')[0]
-        release_path = f"/data/web_static/releases/{archive_name}/"
-        tmp_path = f'/tmp/{archive_name}.tgz'
+        ar_name = archive_path.replace('/', ' ')
+        ar_name = shlex.split(ar_name)
+        ar_name = ar_name[-1]
 
-        put(archive_path, '/tmp/')
-        run(f'mkdir -p {release_path}')
-        run(f'tar -xzf {tmp_path} -C {release_path}')
-        run(f'rm -f {tmp_path}')
-        # move uncompressed content out of its uncompressed directory
-        run(f'mv {release_path}web_static/* {release_path}')
-        # rm the empty uncompressed directory web_static
-        run(f'rm -rf {release_path}web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {release_path} /data/web_static/current')
+        bare_ar_name = ar_name.replace('.', ' ')
+        bare_ar_name = shlex.split(bare_ar_name)
+        bare_ar_name = bare_ar_name[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(bare_ar_name)
+        tmp_path = "/tmp/{}".format(ar_name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
     except Exception:
