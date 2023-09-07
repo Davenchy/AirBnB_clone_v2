d70f69f54f20bb2b572488baa138529d6aa4413c
Fadi Asaad <firon1222@gmail.com>
2023-09-07 20:33:24 +0300

    clean partner files, to work on project 0x03 - deploy static

--- 101-setup_web_static.pp
+++
@@ -1,6 +0,0 @@
-# sets up my web servers for the deployment of web_static
-
-exec { 'deploy':
-  provider => shell,
-  command  => "apt-get -y update ; apt-get -y install nginx ; service nginx start ; mkdir -p /data/web_static/shared/ ; mkdir -p /data/web_static/releases/test/ ; echo 'Alx School' > /data/web_static/releases/test/index.html ; ln -sf /data/web_static/releases/test/ /data/web_static/current ; chown -R ubuntu:ubuntu /data/ ; sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default ; service nginx restart"
-}
