06b1c0060e8122cf3e74fdbf2bc0d0e02b432526
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-07 14:19:21 +0000

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module generates a .tgz archive
-from the contents of the web_static folder.
+This module deploys the web_static archive to the servers.
 """
 
 from os.path import exists
 from fabric.api import *
+import shlex
 
 env.hosts = ['54.237.43.214', '54.208.103.110']
 env.user = 'ubuntu'
@@ -17,22 +17,23 @@ def do_deploy(archive_path):
         return False
 
     try:
-        archive_name = str(archive_path).split('/')[-1].split('.')[0]
+        archive_name = archive_path.split('/')[-1].split('.')[0]
         release_path = f"/data/web_static/releases/{archive_name}/"
         tmp_path = f'/tmp/{archive_name}.tgz'
 
         put(archive_path, '/tmp/')
-        run(f'tar -xzvf {tmp_path} -C {release_path}')
+        run(f'mkdir -p {release_path}')
+        run(f'tar -xzf {tmp_path} -C {release_path}')
         run(f'rm -f {tmp_path}')
         # move uncompressed content out of its uncompressed directory
         run(f'mv {release_path}web_static/* {release_path}')
         # rm the empty uncompressed directory web_static
         run(f'rm -rf {release_path}web_static')
-        run(f'rm -f /data/web_static/current')
+        run(f'rm -rf /data/web_static/current')
         run(f'ln -s {release_path} /data/web_static/current')
         print("New version deployed!")
         return True
     except Exception:
         return False
 
-do_deploy('versions/web_static_20170315003959.tgz')
+# do_deploy('/home/vagrant/AirBnB_clone_v2/versions/web_static_20230907130838.tgz')
