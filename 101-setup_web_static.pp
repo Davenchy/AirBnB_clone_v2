a8fa98e4b9d6a00bb97916fd04259727382c0914
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-07 14:52:00 +0000

    fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,5 +2,5 @@
 
 exec { 'deploy':
   provider => shell,
-  command  => "apt-get -y update ; apt-get -y install nginx ; service nginx start ; mkdir -p /data/web_static/shared/ ; mkdir -p /data/web_static/releases/test/ ; echo "Alx School" > /data/web_static/releases/test/index.html ; ln -sf /data/web_static/releases/test/ /data/web_static/current ; chown -R ubuntu:ubuntu /data/ ; sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default ; service nginx restart"
+  command  => "apt-get -y update ; apt-get -y install nginx ; service nginx start ; mkdir -p /data/web_static/shared/ ; mkdir -p /data/web_static/releases/test/ ; echo 'Alx School' > /data/web_static/releases/test/index.html ; ln -sf /data/web_static/releases/test/ /data/web_static/current ; chown -R ubuntu:ubuntu /data/ ; sed -i '41i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default ; service nginx restart"
 }
