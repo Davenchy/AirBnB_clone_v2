fda7c81f4b3c4389ffe87f36415a0c47eda87057
Fadi Asaad <firon1222@gmail.com>
2023-09-10 18:02:44 +0300

    undo changes for task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from os import mkdir
 from datetime import datetime
 
 env.user = 'ubuntu'  # servers username
-# env.hosts = ['54.162.238.187', '54.175.189.248']  # servers ip addresses
+env.hosts = ['54.162.238.187', '54.175.189.248']  # servers ip addresses
 
 
 def do_pack():
@@ -59,7 +59,7 @@ def do_deploy(archive_path):
         sudo(f"tar -xzf {tmp_path} -C {release_path}")
         sudo(f"rm {tmp_path}")
         sudo(f"mv {release_path}web_static/* {release_path}")
-        # sudo(f"rm -rf {release_path}web_static")
+        sudo(f"rm -rf {release_path}web_static")
         sudo("rm -rf /data/web_static/current")
         sudo(f"ln -s {release_path} /data/web_static/current")
         print('New version deployed!')
