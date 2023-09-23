1bcbe496bd04e73071f631a010d38261d901584d
Fadi Asaad <firon1222@gmail.com>
2023-09-23 04:25:09 +0300

    add task 8, create flask web server to fetch data from database

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Host database states sample data using template"""
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list_route():
+    """States list route"""
+    states = storage.all('State').values()
+    storage.close()  # close the db session
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
